8659a7b12390f218cf33a57b15f37e21122a5e72
Christophe Promé <chprome@gmail.com>
2013-06-05 17:18:29 +0200

    servir les ressources statiques

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Map;
 
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.deploy.Verticle;
 
 public class Server extends Verticle {
@@ -11,15 +15,79 @@ public class Server extends Verticle {
     	System.out.println("start server");
         HttpServer server = vertx.createHttpServer();
 
-        server.requestHandler(new Handler<HttpServerRequest>() {
-            public void handle(HttpServerRequest request) {
-                StringBuilder sb = new StringBuilder();
-                for (Map.Entry<String, String> header : request.headers().entrySet()) {
-                    sb.append(header.getKey()).append(": ").append(header.getValue()).append("\n");
-                }
-                request.response.putHeader("content-type", "text/plain");
-                request.response.end(sb.toString());
+        RouteMatcher rm = new RouteMatcher();
+        
+        rm.getWithRegEx(".*\\.(js|css)", new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+            	String fileName = req.path;
+            	System.out.println("JS request: " + fileName);
+            	req.response.sendFile(fileName.substring(1));
             }
-        }).listen(8080, "localhost");
+        });
+        
+        rm.getWithRegEx("/images/.*", new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+            	String fileName = req.path;
+            	req.response.sendFile(fileName.substring(1));
+            }
+        });
+        
+        rm.get("/", new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                BufferedReader reader = null;
+                try {
+                    reader = new BufferedReader(new FileReader("index.html"));
+                    StringBuilder sb = new StringBuilder();
+                    String line = reader.readLine();
+                    while (line != null) {
+                        sb.append(line);
+                        line = reader.readLine();
+                    }
+                    req.response.putHeader("Content-Type", "text/html");
+                    req.response.end(sb.toString());
+
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        
+        rm.get("/trollers", new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                BufferedReader reader = null;
+                try {
+                    reader = new BufferedReader(new FileReader("resources/example.json"));
+                    StringBuilder sb = new StringBuilder();
+                    String line = reader.readLine();
+                    while (line != null) {
+                        sb.append(line);
+                        line = reader.readLine();
+                    }
+                    req.response.putHeader("Content-Type", "application/json");
+                    req.response.end(sb.toString());
+
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+        
+        server.requestHandler(rm).listen(8080, "localhost");
     }
 }
